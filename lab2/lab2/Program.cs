fcee475601e17f7de8152b0be36efb65c3306430
mburly <mburleson@cfl.rr.com>
2022-05-24 10:01:24 -0400

    question 1

--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -7,6 +7,12 @@
             Console.WriteLine("*=*=*=*=*=*=*=*=*=*=*=*=*=*=*");
             Console.WriteLine("Welcome to the Lab 2 Program!");
             Console.WriteLine("*=*=*=*=*=*=*=*=*=*=*=*=*=*=*");
+            Console.WriteLine("Please enter one number to add:");
+            var num1 = Console.ReadLine();
+            Console.WriteLine("Please enter the other number you wish to add:");
+            var num2 = Console.ReadLine();
+            int sum = Int32.Parse(num1) + Int32.Parse(num2);
+            Console.WriteLine($"{num1} + {num2} = {sum}");
         }
     }
 }
