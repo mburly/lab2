87b0cdbd16a416c46e75e3ef2de49bb388ba1ab7
mburly <mburleson@cfl.rr.com>
2022-05-24 09:52:30 -0400

    base code

---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*=*=*=*=*=*=*=*=*=*=*=*=*=*=*");
+            Console.WriteLine("Welcome to the Lab 2 Program!");
+            Console.WriteLine("*=*=*=*=*=*=*=*=*=*=*=*=*=*=*");
+        }
+    }
+}
